7990d47ae2a577da0a70436999a67fd21ae6ae88
ExLZyor <brice.vachez@insa-lyon.fr>
2021-08-02 11:32:27 +0200

    Indexuru

--- slides.js
+++ slides.js
@@ -1,9 +1,13 @@
 jvSlidesIndex = 0;
 logSlidesIndex = 0;
 
+function showSlides() {
+    showJVSlides(jvSlidesIndex);
+    showLogSlides(logSlidesIndex);
+}
 
 function showJVSlides(newPotentialSlide) {
-    let jvSlides = document.getElementsByClassName("slide_jv");
+    let jvSlides = document.getElementById("jeux_videos").getElementsByClassName("slide");
     let dots = document.getElementById("jeux_videos").getElementsByClassName("dot");
     console.log(jvSlides.length);
     jvSlidesIndex = newPotentialSlide % jvSlides.length;
@@ -28,6 +32,31 @@ function showJVSlides(newPotentialSlide) {
     dots[jvSlidesIndex].className += " active";
 }
 
+function showLogSlides(newPotentialSlide) {
+    let logSlides = document.getElementById("logiciels").getElementsByClassName("slide");
+    let dots = document.getElementById("logiciels").getElementsByClassName("dot");
+    logSlidesIndex = newPotentialSlide % logSlides.length;
+
+    var i;
+    for(i = 0; i < logSlides.length; ++i){
+        logSlides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    var id2 = (logSlidesIndex + 1) % logSlides.length;
+    var elem1 = document.getElementById("log" + logSlidesIndex);
+    var elem2 = document.getElementById("log" + id2);
+
+    elem1.parentNode.insertBefore(elem1, elem2);
+
+    elem1.style.display = "inline-block";
+    elem2.style.display = "inline-block";
+
+    dots[logSlidesIndex].className += " active";
+}
+
 function showNextJVSlides() {
     showJVSlides(jvSlidesIndex += 1)
 }
@@ -36,6 +65,20 @@ function showPreviousJVSlides() {
     showJVSlides(jvSlidesIndex -= 1)
 }
 
-function showExplicitSlides(slideIndex) {
+function showExplicitJVSlides(slideIndex) {
     showJVSlides(jvSlidesIndex = slideIndex)
+}
+
+
+
+function showNextLogSlides() {
+    showLogSlides(logSlidesIndex += 1)
+}
+
+function showPreviousLogSlides() {
+    showLogSlides(logSlidesIndex -= 1)
+}
+
+function showExplicitLogSlides(slideIndex) {
+    showLogSlides(logSlidesIndex = slideIndex)
 }
